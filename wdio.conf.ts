723a92dd8ce73d45b9a9fa398b5942c1e78057ad
Shantanu Pradhan <39673018+shantanu89@users.noreply.github.com>
2022-04-11 20:17:41 +0530

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -83,7 +83,7 @@ export const config: WebdriverIO.Config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome',
+        browserName: 'firefox',
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
@@ -137,7 +137,7 @@ export const config: WebdriverIO.Config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['firefoxdriver'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
